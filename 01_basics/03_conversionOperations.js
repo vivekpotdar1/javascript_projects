a81fafd66a2ac6cf8d011e237061b083f58dfafb
vivekpotdar1 <149235327+vivekpotdar1@users.noreply.github.com>
2023-10-29 02:30:31 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+//let score = "33aa"
+/*let score = null
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+*/
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "vivek" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
