4f4a2de0e13caa2c6a6df9a0567a66f3bd7b26ff
vivekpotdar1 <149235327+vivekpotdar1@users.noreply.github.com>
2023-10-29 07:33:21 +0000

    Comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,5 +26,26 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//*************************************************Operations************************** */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+console.log(null > 0)
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0 );
+
+// ===
+
+console.log("2" === 2); // false dataType is not matching
