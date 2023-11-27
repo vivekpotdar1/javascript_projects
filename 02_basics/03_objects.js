127ac0cdb3b26ae40e3d6bda45516f129cbe7dfe
vivekpotdar1 <149235327+vivekpotdar1@users.noreply.github.com>
2023-11-27 19:24:57 +0000

    objects part 3

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//Object de-structure
+const course ={
+    courseName : "js",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+
+const {courseInstructor : instructor} = course
+
+console.log(instructor);
+
+{
+    "name" : "vivek potdar"
+    "age" : 28
+    
+}
