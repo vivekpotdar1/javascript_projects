2481a462b93881e7af1995923facf164a5dd251e
vivekpotdar1 <149235327+vivekpotdar1@users.noreply.github.com>
2023-11-27 18:02:30 +0000

    array_2 final code

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -6,8 +6,23 @@ const dc_heros = ["superman","flash","batman"];
 // console.log(marvel_heros);
 
 const allHeros = marvel_heros.concat(dc_heros);
-console.log(allHeros);
+//console.log(allHeros);
 
 const all_new_heros = [...marvel_heros,...dc_heros];
 
-console.log(all_new_heros);
+//console.log(all_new_heros);
+
+const another_array = [1,2,3,4,[6,7,8,[1,3,6,[9,8,6]]]];
+
+const real_another_array = another_array.flat(Infinity);
+//console.log(real_another_array);
+
+console.log(Array.isArray("Vivek"));
+console.log(Array.from("Vivek"));
+console.log(Array.from({name : "Vivek"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
